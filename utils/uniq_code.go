e593c55755e5598fcd516909130862a7a2583b76
Addision <laijia2008@126.com>
2022-04-05 21:27:54 +0800

    uniq_code.go 加锁

--- utils/uniq_code.go
+++ utils/uniq_code.go
@@ -1,9 +1,12 @@
 package utils
 
+import "sync"
+
 type uniqcode struct {
 	key uint
 	index uint
 	timeSec uint64
+	genMutex sync.Mutex
 }
 
 func NewUniqCode(key uint) *uniqcode {
@@ -16,6 +19,8 @@ func NewUniqCode(key uint) *uniqcode {
 }
 
 func (u *uniqcode) Gen() uint64 {
+	u.genMutex.Lock()
+	defer u.genMutex.Unlock()
 	code := u.timeSec << 32 | uint64(u.key << 16) | uint64(u.index & 0xFFFF)
 	u.index++
 	if u.index > 0xFFFF {
